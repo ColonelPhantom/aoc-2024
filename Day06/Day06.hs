e73ab78f8d575b2fc07e410a30eead98f7793957
Quinten Kock <quintenkock@gmail.com>
2024-12-19 02:45:42 +0100

    day06: also still slow, but optimized quite a bit

--- Day06/Day06.hs
+++ Day06/Day06.hs
@@ -2,6 +2,7 @@ import Data.Maybe (catMaybes)
 import qualified Data.Set as S
 import Prelude hiding (Left, Right)
 import Data.List (unfoldr)
+import qualified Data.Bifunctor as Bf
 
 data Dir = Up | Down | Left | Right deriving (Eq, Ord, Show)
 
@@ -29,22 +30,15 @@ move (Right,x,y)= (x+1,y)
 
 outOfBounds (m,n) (x,y) = x < 0 || x >= m || y < 0 || y >= n
 
-step :: S.Set (Int, Int) -> (Int, Int) -> (Dir, Int, Int) -> Maybe (Dir, Int, Int)
+step :: S.Set (Int, Int) -> (Int, Int) -> (Dir, Int, Int) -> Maybe ((Dir, Int, Int), Bool)
 step obst dims pos@(dir, x, y)
-    | move pos `S.member` obst = step obst dims (right dir, x, y)
+    | move pos `S.member` obst = Bf.second (const True) <$> step obst dims (right dir, x, y)
     | outOfBounds dims (move pos) = Nothing
-    | otherwise = let (nx,ny) = move pos in Just (dir,nx,ny)
+    | otherwise = let (nx,ny) = move pos in Just ((dir,nx,ny), False)
 
--- loops :: Ord a => S.Set a -> (a -> Maybe a) -> a -> Bool
--- loops s f x
---     | x `S.member` s = True
---     | otherwise = case f x of
---                        Just x' -> loops (S.insert x s) f x'
---                        Nothing -> False
-
-loops :: Ord a => S.Set a -> (a -> Maybe a) -> a -> Bool
+loops :: Ord a => S.Set a -> (a -> Maybe (a, Bool)) -> a -> Bool
 loops s f x = case f x of
-                   Just y -> S.member y s || loops (S.insert y s) f y
+                   Just (y, significant) -> if significant then S.member y s || loops (S.insert y s) f y else loops s f y
                    Nothing -> False
 
 main = do
@@ -52,12 +46,11 @@ main = do
     let (sx,sy) = starting input
     let dims@(m,n) = (length (head input), length input)
     let obst = S.fromList (obstacles input)
-    let step'' pos = (\(dir,x,y) -> ((x,y), (dir,x,y))) <$> step obst dims pos
+    let step'' pos = (\(dir,x,y) -> ((x,y), (dir,x,y))) . fst <$> step obst dims pos
     let positions = unfoldr step'' (Up,sx,sy)
     let posmap = S.fromList positions
     putStr "part 1: " >> print (S.size posmap)
 
     let newmaps = S.toList $ S.fromList $ [S.insert (x,y) obst | (x,y) <- S.toList posmap, (x,y) /= (sx,sy)]
     let loopmaps = filter (\o -> loops S.empty (step o dims) (Up,sx,sy)) newmaps
---     mapM_ print loopmaps
     putStr "part 2: " >> print (S.size (S.fromList loopmaps))
