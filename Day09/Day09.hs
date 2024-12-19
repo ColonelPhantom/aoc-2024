e01ccc42366659b042a785819517cf843b331bde
Quinten Kock <quintenkock@gmail.com>
2024-12-19 02:25:05 +0100

    day09: still slow, but optimized a bit

--- Day09/Day09.hs
+++ Day09/Day09.hs
@@ -3,8 +3,6 @@ import Data.Maybe (isJust, isNothing, fromMaybe)
 import Data.Sequence ( (<|), dropWhileR, fromList, Seq(..) )
 import Data.Foldable (Foldable(toList))
 
-traceShow _ x = x
-
 compact :: Seq (Maybe Int) -> Seq Int
 compact Empty = Empty
 compact (Just x :<| xs) = x <| compact xs
@@ -26,19 +24,20 @@ parseBlocks idx [x] = [(Just idx, read [x])]
 parseBlocks idx (x:y:xs) = (Just idx, read [x]) : (Nothing, read [y]) : parseBlocks (idx+1) xs
 
 defrag :: [(Maybe Int, Int)] -> [(Maybe Int, Int)]
-defrag blocks = foldr process1 blocks candidates where
-    process1 cmd block = let result = mergeAll $ defrag1 cmd block in traceShow (cmd, length result) result
+defrag blocks = foldr defrag1 blocks candidates where
     candidates = map (\(Just id, len) -> (id,len)) $ filter (\(x,y) -> isJust x) blocks
+
     defrag1 (id, len) ((Nothing, free):xs) = case compare free len of
         LT -> (Nothing, free) : defrag1 (id, len) xs
         EQ -> (Just id, len) : delete1 (id, len) xs
         GT -> (Just id, len) : (Nothing, free-len) : delete1 (id, len) xs
     defrag1 (id, len) disk@((Just id', l):xs) = if id == id' then disk else (Just id', l) : defrag1 (id, len) xs
-    mergeAll [] = []
-    mergeAll [x] = [x]
-    mergeAll ((Nothing,l) : (Nothing, l') : xs) = mergeAll $ (Nothing, l+l') : xs
-    mergeAll (x:xs) = x : mergeAll xs
-    delete1 (id, len) = map (\x -> if x == (Just id,len) then (Nothing, len) else x)
+
+    delete1 (id, len) [] = []
+    delete1 (id, len) (x:xs) = if x == (Just id, len) then merge1 ((Nothing, len) : xs) else x : delete1 (id, len) xs
+
+    merge1 ((Nothing,l) : (Nothing, l') : xs) = (Nothing, l+l') : xs
+    merge1 xs = xs
 
 checksum :: [Int] -> Int
 checksum = sum . zipWith (*) [0..]
