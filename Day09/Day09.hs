a0df4007fc1303862efbf04716fb5ff381dd2cc6
Quinten Kock <quintenkock@gmail.com>
2024-12-27 22:56:45 +0100

    day9: use sequence

--- Day09/Day09.hs
+++ Day09/Day09.hs
@@ -1,6 +1,6 @@
 import Data.Maybe (isJust, isNothing, fromMaybe)
 -- import Debug.Trace (traceShowId, traceShow)
-import Data.Sequence ( (<|), dropWhileR, fromList, Seq(..) )
+import Data.Sequence ( (<|), (|>), dropWhileR, fromList, Seq(..), breakl, (><) )
 import Data.Foldable (Foldable(toList))
 
 compact :: Seq (Maybe Int) -> Seq Int
@@ -23,21 +23,30 @@ parseBlocks idx [] = []
 parseBlocks idx [x] = [(Just idx, read [x])]
 parseBlocks idx (x:y:xs) = (Just idx, read [x]) : (Nothing, read [y]) : parseBlocks (idx+1) xs
 
-defrag :: [(Maybe Int, Int)] -> [(Maybe Int, Int)]
-defrag blocks = foldr defrag1 blocks candidates where
-    candidates = map (\(Just id, len) -> (id,len)) $ filter (\(x,y) -> isJust x) blocks
+data DefragBlock = Free | Orig Int | Used Int deriving Eq
 
-    defrag1 (id, len) ((Nothing, free):xs) = case compare free len of
-        LT -> (Nothing, free) : defrag1 (id, len) xs
-        EQ -> (Just id, len) : delete1 (id, len) xs
-        GT -> (Just id, len) : (Nothing, free-len) : delete1 (id, len) xs
-    defrag1 (id, len) disk@((Just id', l):xs) = if id == id' then disk else (Just id', l) : defrag1 (id, len) xs
+defrag :: Seq (DefragBlock, Int) -> Seq (DefragBlock, Int)
+defrag Empty = Empty
+defrag (disk :|> (Free, len)) = defrag disk |> (Free, len) -- remove trailing empty blocks
+defrag (disk :|> (Used id, len)) = defrag disk |> (Used id, len) -- skip over used blocks
+defrag (disk :|> (Orig id, len)) = case ({-# SCC "defrag/breakl" #-} breakl (\(c,l) -> c == Free && l >= len) disk) of
+    (_, Empty) -> defrag disk |> (Used id, len) -- no free space, skip over
+    (hd, (Free, free) :<| tl) -> case compare free len of
+        LT -> error "boom"
+        EQ -> defrag $ hd >< (Used id, len) <| merge (tl |> (Free, len))
+        GT -> defrag $ hd >< (Used id, len) <| merge (merge ((Free, free-len) <| tl) |> (Free, len))
+    where
+    merge ((Free, l1) :<| (Free, l2) :<| xs) = (Free, l1+l2) <| xs
+    merge (xs :|> (Free, l1) :|> (Free, l2)) = xs |> (Free, l1+l2)
+    merge xs = xs
 
-    delete1 (id, len) [] = []
-    delete1 (id, len) (x:xs) = if x == (Just id, len) then merge1 ((Nothing, len) : xs) else x : delete1 (id, len) xs
-
-    merge1 ((Nothing,l) : (Nothing, l') : xs) = (Nothing, l+l') : xs
-    merge1 xs = xs
+defragList :: [(Maybe Int, Int)] -> [(Maybe Int, Int)]
+defragList = map fromBlock . toList . defrag . fromList . map toBlock where
+    toBlock (Nothing, len) = (Free, len)
+    toBlock (Just id, len) = (Orig id, len)
+    fromBlock (Free, len) = (Nothing, len)
+    fromBlock (Used id, len) = (Just id, len)
+    fromBlock (Orig id, len) = (Just id, len)
 
 checksum :: [Int] -> Int
 checksum = sum . zipWith (*) [0..]
@@ -50,4 +59,4 @@ flatten ((Just id, len):xs) = replicate len id ++ flatten xs
 main = do
     input <- getContents
     putStr "part 1: " >> print (checksum $ compactList $ parse 0 input)
-    putStr "part 2: " >> print (checksum $ flatten $ defrag $ parseBlocks 0 input)
+    putStr "part 2: " >> print (checksum $ flatten $ defragList $ parseBlocks 0 input)
