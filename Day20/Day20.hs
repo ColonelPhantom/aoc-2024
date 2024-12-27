f7b422675bd7a74047283d5a736f13bc4ded5461
Quinten Kock <quintenkock@gmail.com>
2024-12-27 03:04:07 +0100

    day20: simpler and faster!

--- Day20/Day20.hs
+++ Day20/Day20.hs
@@ -1,14 +1,12 @@
 import qualified Data.Map as M
-import qualified Data.Bifunctor as BiF
 import Data.Maybe (catMaybes)
 import qualified Data.Set as S
-import qualified Data.Array as A
-import qualified Data.Map.Merge.Lazy as MM
 import Control.Parallel.Strategies
+import Data.List (unfoldr)
 
 type Coord = (Int, Int)
 data Tile = Empty | Wall | Start | End deriving (Show, Eq)
-type Neighs a = a -> [(a, Int)]
+type Neighs a = a -> [a]
 
 parseBoard :: [String] -> M.Map Coord Tile
 parseBoard xs = M.fromList elems where
@@ -24,79 +22,37 @@ parseBoard xs = M.fromList elems where
 
 findStart :: M.Map a Tile -> a
 findStart board = let [(c,_)] = filter ((== Start) . snd) $ M.toList board in c
-findEnd :: M.Map a Tile -> a
-findEnd board = let [(c,_)] = filter ((== End) . snd) $ M.toList board in c
 
-bfs :: (Ord k, Show k) => Neighs k -> [(k, Int)] -> M.Map k Int -> M.Map k Int
-bfs f [] visited = visited
-bfs f ((q, dist):qs) visited = if new
-    then bfs f queue' visited'
-    else bfs f qs visited
-    where
-        new = M.notMember q visited || dist < visited M.! q
-        neighs = map (BiF.second (dist +)) (f q)
-        visited' = M.insertWith min q dist visited
-        queue' = qs ++ neighs
+path :: M.Map Coord Tile -> [Coord]
+path m = unfoldr step (findStart m, S.empty) where
+    step (c,visited) = case filter (`S.notMember` visited) (neighs m c) of
+        [] -> if S.member c visited then Nothing else Just (c, (c, S.insert c visited))
+        [x] -> Just (c, (x, S.insert c visited))
+        xs -> error $ "more than one path: " ++ show xs
 
 neighs :: M.Map Coord Tile -> Neighs Coord
-neighs m (x,y) = map (,1) $ filter isValid [(x-1, y), (x+1, y), (x, y-1), (x, y+1)] where
+neighs m (x,y) = filter isValid [(x-1, y), (x+1, y), (x, y-1), (x, y+1)] where
     isValid c = M.member c m && m M.! c /= Wall
 
-getCheats :: M.Map Coord Tile -> [(Coord, Coord)]
-getCheats m = do
-    start@(x,y) <- M.keys $ M.filter (/= Wall) m
-    step1 <- [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-    (finish, _) <- neighs m step1
-    if M.member finish m && m M.! finish /= Wall
-        then return (start, finish)
-        else []
+manhattan :: Coord -> Coord -> Int
+manhattan (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
 
--- cheatable :: M.Map Coord Tile -> Int -> Coord -> S.Set Coord
--- cheatable m 0 (x,y) = if M.member (x,y) m then S.singleton (x,y) else S.empty
--- cheatable m n (x,y) = S.unions
---                     $ map (cheatable m (n-1)) [(x,y), (x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+pairs :: [a] -> [(a, [a])]
+pairs [] = []
+pairs (x:xs) = (x,xs) : pairs xs
 
-cheatable :: M.Map Coord Tile -> Int -> [(Coord, M.Map Coord Int)]
-cheatable m len = A.assocs final `using` parListChunk 16 rdeepseq where
-    (minx, miny) = minimum $ M.keys m
-    (maxx, maxy) = maximum $ M.keys m
+cheatable :: [Coord] -> Int -> [(Int, Coord, Coord)]
+cheatable p len = maps where
+    maps = concatMap (\(x,xs) -> [(m,x,y) | y <- xs, let m = manhattan x y, m <= len]) (pairs p)
 
-    final = A.array ((minx, miny), (maxx, maxy)) [((x,y), getFinal (x,y)) | x <- [minx..maxx], y <- [miny..maxy]]
-
-    getFinal :: Coord -> M.Map Coord Int
-    getFinal (x,y) = M.fromListWith (\a b -> b) $ concatMap (\d -> map (,d) $ S.toList $ memo A.! (d,x,y)) [1..len]
-
-    memo :: A.Array (Int, Int, Int) (S.Set Coord)
-    memo = A.array ((0, minx, miny), (len, maxx, maxy)) [((n, x,y), go (x,y) n) | n <- [0..len], x <- [minx..maxx], y <- [miny..maxy]]
-
-    go (x,y) n
-        | M.notMember (x,y) m = S.empty
-        | n <= 0 = if m M.! (x,y) /= Wall then S.singleton (x,y) else S.empty
-        | otherwise = S.unions $ map go1 [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-        where
-            go1 (x', y') = memo `safeLookup` (n-1, x', y')
-            safeLookup a (n', x,y) = if x < minx || x > maxx || y < miny || y > maxy then S.empty else a A.! (n', x,y)
-
-getCheatsN :: Int -> M.Map Coord Tile -> [(Int, Coord, Coord)]
-getCheatsN len m = do
-    (from, tos) <- cheatable m len
-    (to, dist) <- M.toList tos
-    if M.member from m && M.member to m && m M.! from /= Wall && m M.! to /= Wall
-        then return (dist, from, to)
-        else []
-
-solve :: M.Map Coord Tile -> Int -> Int
-solve m len = length $ filter ((>=100) . cheatSavings) cheats where
-    start = findStart m
-    end = findEnd m
-    forward = bfs (neighs m) [(start, 0)] M.empty
-    backward = bfs (neighs m) [(end, 0)] M.empty
-    normalCost = forward M.! end
-    cheats = getCheatsN len m
-    cheatCost (dist, from, to) = forward M.! from + dist + backward M.! to
-    cheatSavings c = normalCost - cheatCost c
+solve :: [Coord]-> Int -> Int
+solve p len = length $ filter ((>=100) . cheatSavings) cheats where
+    times = M.fromList $ zip p [0..]
+    cheats = cheatable p len
+    cheatSavings (dist, from, to) = (times M.! to - times M.! from) - dist
 
 main = do
     board <- parseBoard . lines <$> getContents
-    putStr "part 1: "; print $ solve board 2
-    putStr "part 2: "; print $ solve board 20
+    let p = path board
+    putStr "part 1: "; print $ solve p 2
+    putStr "part 2: "; print $ solve p 20
