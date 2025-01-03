bb973d93bd640d3a881203ccbc31112ce26c8f6d
Quinten Kock <quintenkock@gmail.com>
2025-01-03 02:01:42 +0100

    day22: optimize by using Data.Array

--- Day22/Day22.hs
+++ Day22/Day22.hs
@@ -1,11 +1,8 @@
+import qualified Data.Array as A
+import qualified Data.HashMap.Strict as HM
 import Data.Bits (xor)
-import qualified Data.HashMap.Strict as M
-import qualified Data.IntMap as IM
 import Data.Tuple (swap)
-import Control.Parallel (par)
-import Control.Parallel.Strategies
-import System.Mem (performMajorGC)
-
+import Control.Parallel.Strategies ( parList, rseq, using )
 
 secrets :: Int -> Int
 secrets = step (*2048) . step (`div` 32) . step (*64) where
@@ -17,47 +14,33 @@ part1 :: Int -> Int
 part1 initial = iterate secrets initial !! 2000
 
 -- Part 2
-{-# INLINE changes #-}
 changes :: [Int] -> [Int]
 changes xs = zipWith (-) (tail xs) xs
 
-{-# INLINE slices #-}
 slices :: Int -> [Int] -> [[Int]]
 slices n xs = let s = take n xs in if length s < n then [] else s : slices n (tail xs)
 
-combos :: Int -> [(Int, Int)]
-combos secret = zip (toInt <$> slices 4 (changes ps)) (drop 4 ps) where
+combos :: Int -> [((Int, Int, Int, Int), Int)]
+combos secret = zip (toTuple <$> slices 4 (changes ps)) (drop 4 ps) where
     ps = take 2000 $ map (`mod` 10) $ iterate secrets secret
 
-toInt :: [Int] -> Int
-toInt = foldl (\a b -> a * 19 + b + 9) 0
+toTuple :: [Int] -> (Int, Int, Int, Int)
+toTuple [a, b, c, d] = (a, b, c, d)
 
-fromInt :: Int -> [Int]
-fromInt 0 = []
-fromInt x = fromInt (x `div` 19) ++ [x `mod` 19 - 9]
+comboMap :: Int -> HM.HashMap (Int, Int, Int, Int) Int
+comboMap = HM.fromListWith (\a b -> b) . combos
 
-comboMap :: Int -> IM.IntMap Int
-comboMap = IM.fromListWith (\a b -> b) . combos
-
-unionsMergeWith :: (NFData a) => (a -> a -> a) -> [IM.IntMap a] -> IM.IntMap a
-unionsMergeWith f ms = case unionStep f ms `using` parList rdeepseq of 
-    [] -> IM.empty
-    [m] -> m
-    ms' -> unionsMergeWith f ms'
-    where
-    unionStep f [] = []
-    unionStep f [m] = [m]
-    unionStep f (m:n:ms) = IM.unionWith f m n : unionStep f ms
+pricesArray :: [HM.HashMap (Int, Int, Int, Int) Int] -> A.Array (Int, Int, Int, Int) Int
+pricesArray maps = A.accumArray (+) 0 ((-9, -9, -9, -9), (9, 9, 9, 9)) (concatMap HM.toList maps)
 
 main :: IO ()
 main = do
     input <- map read . lines <$> getContents
-    _ <- input `usingIO` rdeepseq
+
     putStr "part 1: "; print $ sum (map part1 input `using` parList rseq)
-    performMajorGC
 
     let maps = map comboMap input `using` parList rseq
-    let map1 = unionsMergeWith (+) maps
-    let (biggest, best) = maximum $ map swap $ IM.toList map1
-    
-    putStr "part 2: "; print (biggest, fromInt best)
+    let arr = pricesArray maps
+    let (biggest, best) = maximum $ map swap $ A.assocs arr
+
+    putStr "part 2: "; print (biggest, best)
